14c9fa185db2fc618abd5994fce448e63c90e496
violetamdm <122279486+violetamdm@users.noreply.github.com>
2023-02-22 14:00:35 +0100

    Update arbolDOM.js

--- arbolDOM.js
+++ arbolDOM.js
@@ -1,2 +1,11 @@
+node = documenthtml.getElementById("p1")
 node.remove()
 node.add()
+function funcion(){
+  contenido
+}
+/*
+En el html seria añadir:
+evento = fincion()
+ejemplo: onclick = funcion()
+*/
